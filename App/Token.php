ea70698b0f98ad1eba2379088e31024f1024b81f
danielSzkotak <daniel.szkotak.programista@gmail.com>
2022-03-28 19:45:21 +0200

    Added Token class for creatinh hashed token

---
+++ App/Token.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App;
+
+/**
+ * Unique random tokens
+ */
+
+ class Token{
+
+   protected $token;
+
+   public function __construct($token_value = null)
+   {
+      if($token_value){
+
+         $this->token = $token_value;
+      } else {
+
+         $this->token = bin2hex(random_bytes(16)); //16 bytes = 128 bit = 32 hex chars
+      }
+   }
+
+   public function getValue(){
+
+      return $this->token;
+   }
+
+   public function getHash(){
+
+      return hash_hmac('sha256', $this->token, \App\Config::SECRET_KEY); //sha256 = 64 chars
+   }
+ }
