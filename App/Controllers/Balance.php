69f22756fad797a439f787b8105a7840236dbb1d
danielSzkotak <daniel.szkotak.programista@gmail.com>
2022-05-06 22:16:37 +0200

    Added Detailed_balance_model and detailedPeriod.html. Added deleting incomes feture

--- App/Controllers/Balance.php
+++ App/Controllers/Balance.php
@@ -4,7 +4,7 @@ namespace App\Controllers;
 
 use App\Auth;
 use App\Models\Balance_model;
-use App\Models\Categories;
+use App\Models\Detailed_balance_model;
 use App\Serviceable;
 use \Core\View;
 
@@ -33,6 +33,7 @@ class Balance extends Authenticated {
         if(isset($_POST['submitBalance'])){
 
         $balance = new Balance_model($_POST);
+
         $balancePeriod = $balance->getBalancePeriod();
 
         switch ($balancePeriod) {
@@ -118,6 +119,129 @@ class Balance extends Authenticated {
       
     } 
 
-    
+    public function detailedBalanceAction()
+    {
 
+        if(isset($_POST['submitBalance'])){
+
+        $detailedBalance = new Detailed_balance_model($_POST);
+        $balance = new Balance_model($_POST);
+        $balancePeriod = $balance->getBalancePeriod();
+
+  
+
+        switch ($balancePeriod) {
+            case "currentMonth":
+
+                $detailedIncomes = $detailedBalance->getCurrentMonthDetailedIncomes($_SESSION['user_id']);
+                $incomes = $balance->getCurrentMonthIncomesBalance($_SESSION['user_id']);
+                $incomesSum = $balance->getCurrentMonthIncomesSum($_SESSION['user_id']);
+                $expenses = $balance->getCurrentMonthExpensesBalance($_SESSION['user_id']);
+                $expensesSum = $balance->getCurrentMonthExpensesSum($_SESSION['user_id']);
+              
+              break;
+            case "previousMonth":
+
+                $incomes = $balance->getPreviousMonthIncomesBalance($_SESSION['user_id']);
+                $incomesSum = $balance->getPreviousMonthIncomesSum($_SESSION['user_id']);
+                $expenses = $balance->getPreviousMonthExpensesBalance($_SESSION['user_id']);
+                $expensesSum = $balance->getPreviousMonthExpensesSum($_SESSION['user_id']);
+
+              break;
+            case "currentYear":
+
+                $incomes = $balance->getCurrentYearIncomesBalance($_SESSION['user_id']);
+                $incomesSum = $balance->getCurrentYearIncomesSum($_SESSION['user_id']);
+                $expenses = $balance->getCurrentYearExpensesBalance($_SESSION['user_id']);
+                $expensesSum = $balance->getCurrentYearExpensesSum($_SESSION['user_id']);
+             
+
+              break;
+            case "non_standard_period":
+              
+                $incomes = $balance->getCustomDatesIncomesBalance($_SESSION['user_id'], $_POST['startDate'], $_POST['endDate']);
+                $incomesSum = $balance->getCustomDatesIncomesSum($_SESSION['user_id'], $_POST['startDate'], $_POST['endDate']);
+                $expenses = $balance->getCustomDatesExpensesBalance($_SESSION['user_id'], $_POST['startDate'], $_POST['endDate']);
+                $expensesSum = $balance->getCustomDatesExpensesSum($_SESSION['user_id'], $_POST['startDate'], $_POST['endDate']);
+
+                break;
+            default:
+             
+          }
+
+          $periods = array (
+            "currentMonth" => "Bieżący miesiąc",
+            "previousMonth" => "Poprzedni miesiąc",
+            "currentYear" => "Bieżący rok",
+            "non_standard_period" => "Wybrany okres"
+          );
+
+         
+
+          if($incomesSum[0]['total'] == NULL && $expensesSum[0]['total'] == NULL){
+            $balanceSum = NULL;
+          } else {
+            $balanceSum = $incomesSum[0]['total'] - $expensesSum[0]['total'];
+            $balanceSum = Serviceable::formatAmountToModal($balanceSum);
+          }
+
+          //$incomes = Serviceable::formatAmountToView($incomes);
+          $incomesSum = Serviceable::formatAmountToView($incomesSum);
+          //$expenses = Serviceable::formatAmountToView($expenses);
+          $expensesSum = Serviceable::formatAmountToView($expensesSum);
+
+
+        View::renderTemplate('Balance/detailedPeriod.html',[
+            'incomes' => $incomes,
+            'detailedIncomes' => $detailedIncomes,
+            'incomesSum' => $incomesSum,
+            'expenses' => $expenses,
+            'expensesSum' => $expensesSum,
+            'periods' => $periods,
+            'balancePeriod' => $balancePeriod,
+            'balanceSum' => $balanceSum,
+            'startDate' => $_POST['startDate'],
+            'endDate' => $_POST['endDate']
+        ]);
+
+        
+    } else {
+
+        //$this->redirect('/balance/period');
+        var_dump('nie udane');
+    }
+      
+    } 
+
+    public function deleteAction(){
+
+      if(isset($_POST['submitDelete'])){
+
+        $detailedBalance = new Detailed_balance_model($_POST);
+        $balance = new Balance_model($_POST);
+        $idToDelete = $_POST['incomeToDeleteID'];
+
+    
+        $deleteIncome = $detailedBalance->deleteIncome($idToDelete);
+        
+        $detailedIncomes = $detailedBalance->getCurrentMonthDetailedIncomes($_SESSION['user_id']);
+        
+        View::renderTemplate('Balance/detailedPeriod.html',[
+          //'incomes' => $incomes,
+          'detailedIncomes' => $detailedIncomes,
+          //'incomesSum' => $incomesSum,
+          //'expenses' => $expenses,
+          //'expensesSum' => $expensesSum,
+          //'periods' => $periods,
+          //'balancePeriod' => $balancePeriod,
+          //'balanceSum' => $balanceSum,
+          //'startDate' => $_POST['startDate'],
+          //'endDate' => $_POST['endDate']
+      ]);
+
+      } else {
+         var_dump('dupa');
+      }
+
+  }
 }
---
+++ App/Models/Detailed_balance_model.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Models;
+
+use PDO;
+
+
+ class Detailed_balance_model extends \Core\Model{
+  
+
+    public function __construct($data = [])
+    {
+        foreach ($data as $key => $value){
+            $this->$key = $value;
+        };
+    }
+    
+    
+    public function getCurrentMonthDetailedIncomes($userID){
+
+        $firstDayOfTheMonth = date('Y-m-01');
+        $lastDayOfTheMonth = date('Y-m-t');
+
+        $sql = "SELECT incomes.id, incomes_category_assigned_to_users.name, incomes.amount, incomes.date_of_income FROM incomes_category_assigned_to_users, incomes WHERE (incomes.date_of_income BETWEEN '$firstDayOfTheMonth' AND '$lastDayOfTheMonth') AND (incomes_category_assigned_to_users.user_id=:userID) AND (incomes_category_assigned_to_users.user_id = incomes.user_id) AND (incomes.income_category_assigned_to_user_id=incomes_category_assigned_to_users.id) ORDER BY incomes.date_of_income DESC;";
+  
+  
+        $db = static::getDB();
+        $stmt = $db->prepare($sql);
+        $stmt->bindValue(':userID', $userID, PDO::PARAM_INT);
+    
+        $stmt->setFetchMode(PDO::FETCH_ASSOC);
+
+        $stmt->execute();
+
+        return $stmt->fetchAll();
+
+     }
+
+     public function deleteIncome($incomeID){
+
+      //$firstDayOfTheMonth = date('Y-m-01');
+      //$lastDayOfTheMonth = date('Y-m-t');
+
+      $sql = "DELETE FROM incomes WHERE incomes.id=:incomeID";
+
+
+      $db = static::getDB();
+      $stmt = $db->prepare($sql);
+      $stmt->bindValue(':incomeID', $incomeID, PDO::PARAM_INT);
+  
+      $stmt->setFetchMode(PDO::FETCH_ASSOC);
+
+      $stmt->execute();
+
+      return $stmt->fetchAll();
+
+   }
+
+     
+}
+
+  
+
+
